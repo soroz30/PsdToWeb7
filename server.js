45a73087451c34637cce59dc4d99a4a0064ebc4d
soroz30 <soroz30@yahoo.pl>
2018-02-18 10:52:37 +0100

    updated npm

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.static('./'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname+'dist/index.html'));
 });
 
 app.listen(3000);
